7de2c726d246350f962abe63f470854529fc09a3
Elias Monteiro <elias.leimt@gmail.com>
2020-03-16 23:34:15 -0300

    aula 5 - padrões REST e POST pt 1

--- devdojo-springboot/src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
+++ devdojo-springboot/src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -1,8 +1,12 @@
 package br.com.devdojo.awesome.endpoint;
 
+import br.com.devdojo.awesome.error.CustomErrorType;
 import br.com.devdojo.awesome.model.Student;
 import br.com.devdojo.awesome.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +17,29 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 @RestController
-@RequestMapping("student")
+@RequestMapping("students") // usar o mapeamento no plural
 public class StudentEndpoint {
+    private final DateUtil dateUtil;
 
     @Autowired
-    private DateUtil dateUtil;
+    public StudentEndpoint(DateUtil dateUtil) { // incluindo o @Autowired no construtor
+        this.dateUtil = dateUtil;               // os avisos (warnings) do IntelliJ desaparecem
+    }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/list")
-    public List<Student> listAll() {
+    @RequestMapping(method = RequestMethod.GET) // remover o path, quando houver apenas um método get
+    public ResponseEntity<?> listAll() {
         //System.out.println( dateUtil.formatLocalDateTimeToDatabaseStyle( LocalDateTime.now() ) );
-        return asList(new Student("Deku"), new Student( "Todoroki"));
+        return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
+        Student student = new Student();
+        student.setId( id );
+
+        int index = Student.studentList.indexOf( student );
+        if (index == -1)
+            return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
     }
 }
--- devdojo-springboot/src/main/java/br/com/devdojo/awesome/model/Student.java
+++ devdojo-springboot/src/main/java/br/com/devdojo/awesome/model/Student.java
@@ -1,13 +1,33 @@
 package br.com.devdojo.awesome.model;
 
+import java.util.ArrayList;
+import static java.util.Arrays.asList;
+import java.util.List;
+import java.util.Objects;
+
 public class Student {
+    private int id;
     private String name;
+    public static List<Student> studentList;
+
+    static {
+        studentRepository();
+    }
 
     public Student(String name) {
         this.name = name;
     }
 
+    public Student(int id, String name) {
+        this(name);
+        this.id = id;
+    }
+
     public Student() {
+    }
+
+    private static void studentRepository() {
+        studentList = new ArrayList<>(asList(new Student(1, "Deku"), new Student( 2,"Todoroki")));
     }
 
     public String getName() {
@@ -16,5 +36,26 @@ public class Student {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
