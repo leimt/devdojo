4f8b918750135b8a5a95a0703b7d3b9a9d728a0e
Elias Monteiro <elias.leimt@gmail.com>
2020-03-31 22:32:31 -0300

    aula 15 - paginação em requisições REST

--- springboot-essentials/src/main/java/br/com/devdojo/repository/StudentRepository.java
+++ springboot-essentials/src/main/java/br/com/devdojo/repository/StudentRepository.java
@@ -2,10 +2,10 @@ package br.com.devdojo.repository;
 
 import br.com.devdojo.model.Student;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
-// https://docs.spring.io/autorepo/docs/spring-data-commons/1.5.3.RELEASE/api/org/springframework/data/repository/CrudRepository.html
-public interface StudentRepository extends CrudRepository<Student, Long> {
-    // não é necessário implementar esta interface: StudentRepository, pois tudo será
-    List<Student> findByNameIgnoreCaseContaining(String name);      // implementado em tempo de execução
+public interface StudentRepository extends PagingAndSortingRepository<Student, Long> {
+
+    List<Student> findByNameIgnoreCaseContaining(String name);
 }
